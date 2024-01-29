d5c95f908441f5bf8284dcf364afdc8e1c0b3531
permana017 <ahdipermana07@gmail.com>
2024-01-29 11:00:27 +0700

    add validation email password not found

--- src/model/auth.model.js
+++ src/model/auth.model.js
@@ -4,25 +4,25 @@ const bcrypt = require('bcrypt');
 
 
 const authModel = {
-    login:({username, password, phone, email, role})=> {
-        return new Promise((resolve, reject)=> {
+    login: ({ username, password, phone, email, role }) => {
+        return new Promise((resolve, reject) => {
             db.query(
                 `SELECT * from users WHERE email=$1`, [email],
                 (err, result) => {
-                if(err) return reject(err.message)
-                if(result.rows.length == 0 ) return reject('email or password not valid')
-                bcrypt.compare(password, result.rows[0].password, 
-                    function(err, hashingResult) {
-                      if(err) return reject(err.message)
-                      if(!hashingResult) return reject("NOT_FOUND")
-                      return resolve(result.rows[0])
-                  });
+                    if (err) return reject(err.message)
+                    if (result.rows.length == 0) return reject('email not found')
+                    bcrypt.compare(password, result.rows[0].password,
+                        function (err, hashingResult) {
+                            if (err) return reject(err.message)
+                            if (!hashingResult) return reject('password not valid')
+                            return resolve(result.rows[0])
+                        });
                 }
-              );
+            );
         })
     },
-    register:({username, password, email, phone})=>{
-        console.log({username, password, email, phone});
+    register: ({ username, password, email, phone }) => {
+        console.log({ username, password, email, phone });
         return new Promise((resolve, reject) => {
             db.query(
                 `INSERT INTO users (id, username, password, email, phone) VALUES ($1, $2, $3, $4, $5)`,
@@ -33,9 +33,9 @@ const authModel = {
                     } else {
                         if (err.code == 23505) {
                             return reject("username or email already exist");
-                            } else {
+                        } else {
                             return resolve("ADD_SUCCESS");
-                            }
+                        }
                     }
                 }
             );
