56e19ff65c6dc5f263b0bd8f02b376d875ef1c85
permana017 <ahdipermana07@gmail.com>
2024-06-01 11:04:11 +0700

    add delete image ini controller product

--- helper/verifyToken.js
+++ helper/verifyToken.js
@@ -1,39 +1,36 @@
-const jwt = require('jsonwebtoken')
-const {JWT_PRIVATE_KEY} = process.env
+const jwt = require("jsonwebtoken");
+const { JWT_PRIVATE_KEY } = process.env;
 
+const verifyToken = (req, res, next) => {
+  console.log(req.header("token"));
+  const token = req.header("token");
+  if (!req.header("token")) {
+    return res.status(400).send({
+      message: "token is required",
+    });
+  } else {
+    // console.log(req.header.authorization)
 
-
-const verifyToken = (req, res, next)=> {
-
-    const token = req.header('token')
-    if(!req.header('token')) {
-        return res.status(400).send({
-            message: 'token is required'
-        })
-    }else {
-        // console.log(req.header.authorization)
-        
-        jwt.verify(token, JWT_PRIVATE_KEY, function(err, decoded) {
-            if(!err) {
-                if(decoded.role ==="admin") {
-                    next()
-                }else if(decoded.role ==="user") {
-                    return res.status(403).send({
-                        message: 'kamu tidak memiliki akses.'
-                    })
-                }else {
-                    return res.status(403).send({
-                        message: 'kamu tidak memiliki akses.'
-                    })
-                }
-            }else {
-                return res.status(400).send({
-                    message: 'token tidak valid'
-                })
-            }
+    jwt.verify(token, JWT_PRIVATE_KEY, function (err, decoded) {
+      if (!err) {
+        if (decoded.role === "admin") {
+          next();
+        } else if (decoded.role === "user") {
+          return res.status(403).send({
+            message: "kamu tidak memiliki akses.",
           });
-    }
-}
+        } else {
+          return res.status(403).send({
+            message: "kamu tidak memiliki akses.",
+          });
+        }
+      } else {
+        return res.status(400).send({
+          message: "token tidak valid",
+        });
+      }
+    });
+  }
+};
 
-
-module.exports = verifyToken
+module.exports = verifyToken;
